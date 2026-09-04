d0353e47b6adc628dd1cbbf3b1e49a7a0bab5649
Shahariar Ahmed <74198638+shahariarpo@users.noreply.github.com>
2026-09-04 14:51:01 +0600

    LFS

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import streamlit as st
 import pickle
+import requests
+
+api_key = st.secrets["TMDB_API_KEY"]
+
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US"
+    response = requests.get(url)
+    data = response.json()
+    poster_path = data.get("poster_path")
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500" + poster_path
+    else:
+        return "https://via.placeholder.com/500x750?text=No+Poster"
 
 def recommend(movie):
     movie_index = int(movies_df[movies_df['title'] == movie].index[0])
@@ -7,10 +20,14 @@ def recommend(movie):
     movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
+    recommended_movies_posters = []
     for i in movie_list:
-        recommended_movies.append(movies_df.iloc[i[0]].title)
+        index = i[0]
+        movie_id = movies_df.iloc[index].id
+        recommended_movies.append(movies_df.iloc[index].title)
+        recommended_movies_posters.append(fetch_poster(movie_id))
 
-    return recommended_movies
+    return recommended_movies, recommended_movies_posters
 
 
 similarity = pickle.load(open("similarity.pkl", "rb"))
@@ -18,8 +35,7 @@ movies_df = pickle.load(open("movies.pkl", "rb"))
 movies_list = movies_df['title'].values
 
 st.header("Movie Recommender System", divider=True)
-st.write("Model trained on tmdb_5000_movies dataset.")
-st.write("The tmdb_5000_movies dataset covers movies from 1916 to 2017,with the vast majority being modern releases and a strong skew toward U.S. productions as the dataset contains detailed information about roughly 5,000 movies, mainly from the United States over the past 100 years (1916-2017).")
+st.write("Model trained on tmdb_5000_movies dataset. (1990s - 2010s)")
 
 selected_movie_name = st.selectbox("Enter Movie",
                                    movies_list,
@@ -28,8 +44,26 @@ selected_movie_name = st.selectbox("Enter Movie",
 
 if st.button("Recommend"):
     st.write("Showing Recommendations for:", selected_movie_name)
-    recommendations = recommend(selected_movie_name)
+    names, posters = recommend(selected_movie_name)
 
-    for movie in recommendations:
-        st.write(movie)
+    col1, col2, col3, col4, col5 = st.columns(5)
 
+    with col1:
+        st.image(posters[0])
+        st.text(names[0])
+
+    with col2:
+        st.image(posters[1])
+        st.text(names[1])
+
+    with col3:
+        st.image(posters[2])
+        st.text(names[2])
+
+    with col4:
+        st.image(posters[3])
+        st.text(names[3])
+
+    with col5:
+        st.image(posters[4])
+        st.text(names[4])
